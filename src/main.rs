59b124420a747156b2e34ab780833d04f06c1e3f
geofreybundala <bundalageofrey2@gmail.com>
2022-10-12 13:57:50 +0300

    👌 IMPROVE: implement structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8070".to_string());
+    //listen connection ✨
+    server.run();
+}
+
+struct  Server {
+    addr: String
+}
+
+impl Server {
+    fn new(addr: String) -> Server{
+        Server{
+            addr
+        }
+    } 
+
+    fn run(self){
+
+    }
 }
