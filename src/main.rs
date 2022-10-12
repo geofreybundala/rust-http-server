758a07b2495c6161bd3678ce61a3eade5d840b41
geofreybundala <bundalageofrey2@gmail.com>
2022-10-12 17:33:57 +0300

    👌 IMPROVE: separate logics in modules

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,52 @@
+use server::Server;
+use http::request::Request;
+
 fn main() {
     let server = Server::new("127.0.0.1:8070".to_string());
     //listen connection ✨
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
+mod server {
+    pub struct Server {
+        addr: String,
     }
 
-    fn run(self) {
-        print!("listening on {}", self.addr)
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+
+        pub fn run(self) {
+            print!("listening on {}", self.addr)
+        }
     }
 }
 
-struct Request{
-    path: String,
-    query_string: Option<String>,
-    method: Method
+mod http {
+    pub mod request {
+        use super::method::Method;
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method,
+        }
+    }
+
+    mod method {
+        pub enum Method {
+            GET,
+            PUT,
+            POST,
+            DELETE,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
 }
 
-enum Method {
-    GET,
-    PUT,
-    POST,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-}
+
+
