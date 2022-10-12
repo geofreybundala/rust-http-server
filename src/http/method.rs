470d06d9928de7a5e788734dabd04b20cec940f1
geofreybundala <bundalageofrey2@gmail.com>
2022-10-12 17:47:05 +0300

    👌 IMPROVE: separate module into files

---
+++ src/http/method.rs
@@ -0,0 +1,11 @@
+pub enum Method {
+    GET,
+    PUT,
+    POST,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,18 @@
-use server::Server;
 use http::request::Request;
+use server::Server;
 
+mod server;
+
+mod http;
 fn main() {
     let server = Server::new("127.0.0.1:8070".to_string());
     //listen connection ✨
     server.run();
 }
 
-mod server {
-    pub struct Server {
-        addr: String,
-    }
 
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self { addr }
-        }
 
-        pub fn run(self) {
-            print!("listening on {}", self.addr)
-        }
-    }
-}
 
-mod http {
-    pub mod request {
-        use super::method::Method;
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method,
-        }
-    }
-
-    mod method {
-        pub enum Method {
-            GET,
-            PUT,
-            POST,
-            DELETE,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH,
-        }
-    }
-}
 
 
 
