3e84d7c0b9d69228a37b0f3ffa87a928b12a5dd9
geofreybundala <bundalageofrey2@gmail.com>
2022-10-13 12:12:36 +0300

    👌 IMPROVE: add TCP listener

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+use std::net::TcpListener;
 pub struct Server {
     addr: String,
 }
@@ -8,6 +10,23 @@ impl Server {
     }
 
     pub fn run(self) {
-        print!("listening on {}", self.addr)
+        println!("listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received request: {}",String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => print!("failed to read connection {}", e)
+                    }
+                },
+                Err(e) => println!("Failed to establish connection{}",e)
+                
+            }
+        }
     }
 }
